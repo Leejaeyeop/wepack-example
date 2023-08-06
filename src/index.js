9afa0a90145a6d5877a767d500043b684e30896b
Leejaeyeop <dlwoduq78@naver.com>
2023-08-06 20:19:46 +0900

    lodash 라이브러리 테스트 코드 추가

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-import hello from "./hello.js"
-import world from "./world.js"
-import css from "./style.css"
+import hello from "./hello.js";
+import world from "./world.js";
+import css from "./style.css";
+import * as THREE from "three";
+import _ from "lodash";
 
-document.querySelector("#root").innerHTML = hello + " " + world
-console.log('css',css)
+document.querySelector("#root").innerHTML = hello + " " + world;
+console.log("css", css);
+
+console.log(THREE);
+console.log(_.cloneDeep({ a: "a" }));
